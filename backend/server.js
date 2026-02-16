662379fbb0be0d1ab473838e0612d1035595bd96
Lakshit Tyagi <lakshit175@gmail.com>
2026-02-16 19:22:05 +0530

    Fixed a Bug (Refreshing on /landing page causing nginx error) changed react routing to optional. e.g:- If page not found in /usr/share/nginx/html/landing it switches to ReactRouting.

--- backend/server.js
+++ backend/server.js
@@ -193,4 +193,9 @@ app.get("/", (req, res) => {
 });
 
 // --- Start Server ---
-app.listen(5000, () => console.log("🚀 Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on port ${PORT}`)
+);
+
